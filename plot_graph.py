f2dcc5899340e09d3a042109b51b1199c2978f45
Anagha Jois <anaghasnjois@gmail.com>
2016-05-08 17:08:07 +0200

    importing matplotlib dates

--- plot_graph.py
+++ plot_graph.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt
+import matplotlib.dates as dates
 
 def plot_area_graph(dF, file_name, date_intervals):
     ax = dF.plot.area(title=file_name, xticks= date_intervals);
-    ax.xaxis.set_major_formatter(date_intervals.DateFormatter('%b%d'))
+    ax.xaxis.set_major_formatter(dates.DateFormatter('%b%d'))
     lgd = plt.legend(loc='center left', bbox_to_anchor=(1.0, 0.5))
     fig = ax.get_figure()
     fig.set_size_inches(12.5, 6.5)
